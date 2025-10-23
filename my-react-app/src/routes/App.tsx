c266022c397602f16b427247aad2a840f420c2e3
Gallina Tactica <pepguiemnolla@gmail.com>
2025-10-23 20:32:37 +0200

    Added first stage index

--- my-react-app/src/routes/App.tsx
+++ my-react-app/src/routes/App.tsx
@@ -4,7 +4,20 @@ import '../scss/App.scss'
 function App() {
   return (
     <>
+    <div className='tittle-container'>
+        <img src="/imatges/pikachu.png" alt="" className='tittle-img-pikachu'/>
+        <div className='tittle-text'>
+          <h1>CardTracker</h1>
+          <h2>Pokemon</h2>
+        </div>
+      <img src="/imatges/snorlax.png" alt="" className='tittle-img-snorlax'/>
+    </div>
+    <div className='tracking-plans-main-container'>
+      <p>Tracking plans</p>
+      <div className='tracking-plans-container'>
 
+      </div>
+    </div>  
     </>
   )
 }
